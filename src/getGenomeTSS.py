7d13429cc6d8b081970ff8c9ab9cde6d71d8ec9a
kmualim <kristy.mualim@mail.mcgill.ca>
2020-05-21 18:28:59 -0700

    add ENSEMBL file; grab tss code

--- src/getGenomeTSS.py
+++ src/getGenomeTSS.py
@@ -71,11 +71,12 @@ def process_genome_tss(args):
     tss_file = args.tss_file
     sort_command = "bedtools sort -faidx {chrom_sizes} -i {tss_file} > {tss_file}.sorted; mv {tss_file}.sorted {tss_file}".format(**locals())
     run_command(sort_command)
-
+    print("Finished Running code")
     # sorted tss1kb_file
     tss1kb_file = read_tss_file(args.tss_file)
     tss1kb_file['H3K27ac.RPM'] = tss_df[feature_name+'.RPKM']
 
+    # This loop is taking a while; figure out speed up! 
     # Take top 2 promoters based on counts 
     gene_tss_df = None
     for gene in tss1kb_file['TargetGene']: 
